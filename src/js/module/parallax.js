0f8211c1af89f4ac54f71346c20427e84f59448c
yugp0208 <yuu.fepnhdper2000@gmail.com>
2024-03-30 22:38:21 +0900

    KVの改修

--- src/js/module/parallax.js
+++ src/js/module/parallax.js
@@ -3,5 +3,5 @@ import simpleParallax from 'simple-parallax-js';
 const parallaxElem = document.getElementsByClassName("js-parallax");
 console.log(parallaxElem)
 new simpleParallax(parallaxElem,{
-    scale: 1.2
+    scale: 1.2,
 });
